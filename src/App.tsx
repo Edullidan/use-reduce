2116853127d8df1fb2c2ab28047bab45664ded94
Edullidan <128309694+Edullidan@users.noreply.github.com>
2023-06-03 18:00:06 +0400

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+
+const initialState = {
+  userName: "asd",
+  age: 1234,
+  postMessage: [{ title: "asd" }, {}, {}],
+  likedPosts: [{}],
+  photos: [{}, {}, {}],
+  address: {
+    home: {
+      street: "asdad",
+      building: 134,
+    },
+    work: {
+      building: 134,
+      street: "asdad",
+    },
+  },
+};
+
+type actionType = {
+  type: string;
+  payload: string;
+};
+
+type state = typeof initialState;
+
+const reducer = (state: state, action: actionType) => {
+  const newState = {...state};
+  switch (action.type) {
+    case "UPDATE_WORK_ADDRESS":
+     state.address.work.street = action.payload
+     return newState;
+     case 'UPDATE_USERNAME':
+      state.userName = action.payload as string
+      return newState;
+    default:
+      throw new Error();
+  }
+};
 
 function App() {
+  const [user, dispatch] = useReducer(reducer, initialState);
+
+  console.log(user)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+      <p>Work Address: {user.address.work.street}</p>
+      <p>UserName: {user.userName}</p>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "UPDATE_WORK_ADDRESS",
+            payload: "Vazha-Pshavela",
+          });
+          dispatch({
+            type: 'UPDATE_USERNAME',
+            payload: 'Edo'
+          })
+        }}
+      >
+        Update Street
+      </button>
     </div>
   );
 }
